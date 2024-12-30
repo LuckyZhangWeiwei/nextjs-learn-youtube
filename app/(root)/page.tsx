ec2647ff72638a6092d4669e9ef1c980f6ea98fe
weiwei.zhang <zhangzhang_10@hotmail.com>
2024-12-30 11:10:29 +0800

    completed 2:52:46

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import SearchForm from "@/components/SearchForm";
 import StartupCard, { StartupTypeCard } from "@/components/StartupCard";
-import { client } from "@/sanity/lib/client";
 import { STARTUPS_QUERY } from "@/lib/queries";
+import { sanityFetch, SanityLive } from "@/lib/live";
 
 async function Home({
   searchParams,
@@ -11,7 +11,7 @@ async function Home({
 }) {
   const query = (await searchParams).query;
 
-  const posts = await client.fetch(STARTUPS_QUERY);
+  const { data: posts } = await sanityFetch({ query: STARTUPS_QUERY });
 
   return (
     <>
@@ -39,6 +39,8 @@ async function Home({
           )}
         </ul>
       </section>
+
+      <SanityLive />
     </>
   );
 }
