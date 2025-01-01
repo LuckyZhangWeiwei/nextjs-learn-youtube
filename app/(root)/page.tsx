430ed6961db048dd002a63a0abcf434a65613b9b
weiwei.zhang <zhangzhang_10@hotmail.com>
2025-01-01 17:08:53 +0800

    completed 3:44:14

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,6 +3,7 @@ import SearchForm from "@/components/SearchForm";
 import StartupCard, { StartupTypeCard } from "@/components/StartupCard";
 import { STARTUPS_QUERY } from "@/lib/queries";
 import { sanityFetch, SanityLive } from "@/lib/live";
+import { auth } from "@/auth";
 
 async function Home({
   searchParams,
@@ -12,6 +13,8 @@ async function Home({
   const query = (await searchParams).query;
 
   const params = { search: query || null };
+
+  const session = await auth();
 
   const { data: posts } = await sanityFetch({ query: STARTUPS_QUERY, params });
 
