75620d5f65a5c5b5e423793a18816d785817adfd
weiwei.zhang <zhangzhang_10@hotmail.com>
2025-01-01 10:18:19 +0800

    completed 3:22:40

--- components/View.tsx
+++ components/View.tsx
@@ -1,7 +1,17 @@
 import React from "react";
+import Ping from "@/components/Ping";
 
 function View({ id }: { id: string }) {
-  return <div>View</div>;
+  return (
+    <div className="view-container">
+      <div className="absolute -top-2 -right-2">
+        <Ping />
+      </div>
+      <p className="view-text">
+        <span className="font-black">100 views</span>
+      </p>
+    </div>
+  );
 }
 
 export default View;
