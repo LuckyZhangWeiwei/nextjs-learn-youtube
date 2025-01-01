9d70e390626ab6a65fcf20219ac20e5a1c6c438b
weiwei.zhang <zhangzhang_10@hotmail.com>
2025-01-01 10:34:02 +0800

    completed 3:26:05

--- components/View.tsx
+++ components/View.tsx
@@ -1,14 +1,20 @@
 import React from "react";
 import Ping from "@/components/Ping";
+import { client } from "@/sanity/lib/client";
+import { STARTUP_VIEWS_QUERY } from "@/lib/queries";
 
-function View({ id }: { id: string }) {
+async function View({ id }: { id: string }) {
+  const { views: totalViews } = await client
+    .withConfig({ useCdn: false })
+    .fetch(STARTUP_VIEWS_QUERY, { id });
+
   return (
     <div className="view-container">
       <div className="absolute -top-2 -right-2">
         <Ping />
       </div>
       <p className="view-text">
-        <span className="font-black">100 views</span>
+        <span className="font-black">views: {totalViews}</span>
       </p>
     </div>
   );
