96fd785da224296974716db7d4f9d3bba499d72f
weiwei.zhang <zhangzhang_10@hotmail.com>
2025-01-01 12:16:30 +0800

    completed 3:33:03

--- components/View.tsx
+++ components/View.tsx
@@ -2,11 +2,21 @@ import React from "react";
 import Ping from "@/components/Ping";
 import { client } from "@/sanity/lib/client";
 import { STARTUP_VIEWS_QUERY } from "@/lib/queries";
+import { writeClient } from "@/sanity/lib/write-client";
+import { after } from "next/server";
 
 async function View({ id }: { id: string }) {
   const { views: totalViews } = await client
     .withConfig({ useCdn: false })
     .fetch(STARTUP_VIEWS_QUERY, { id });
+
+  after(
+    async () =>
+      await writeClient
+        .patch(id)
+        .set({ views: totalViews + 1 })
+        .commit()
+  );
 
   return (
     <div className="view-container">
@@ -19,5 +29,4 @@ async function View({ id }: { id: string }) {
     </div>
   );
 }
-
 export default View;
